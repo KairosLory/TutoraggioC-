fd79fc8ddc42a41d07f492d8f77ec1ad2f6f8288
Lorenzo Pedoni <lorenzopedoni53@gmail.com>
2026-09-14 22:30:23 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio1_LoryTutor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Esercizio1_LoryTutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Esercizio2_LoryTutor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Esercizio2_LoryTutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
